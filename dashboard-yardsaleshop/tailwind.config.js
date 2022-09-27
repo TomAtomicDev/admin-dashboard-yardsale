1b565c420a469e1f9453b94b9062fb0d01ea54c4
TomAtomicDev <tompenaloza@gmail.com>
2022-09-27 16:50:19 -0400

    Configured ESLint, Prettier, Tailwind, PostCSS and Built the MainLayout

---
+++ dashboard-yardsaleshop/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const colors= require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/common/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+      },
+    },
+    
+  },
+  plugins: [],
+}
